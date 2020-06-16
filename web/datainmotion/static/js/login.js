7b6cbb3009dc087b54213425d47471ced6be08fd
alejol311 <alejolo311@gmail.com>
2020-06-16 10:26:20 -0500

    finish login feature

--- web/datainmotion/static/js/login.js
+++ web/datainmotion/static/js/login.js
@@ -1,5 +1,23 @@
-let logout = '<a id="myLink" href="#" onclick="logOut();return false;">Logout</a>'
+let htmlLogin =  '<a href="/login"> <span>Login</span> </a>'
+let htmlLogout = `<a>
+                    <span>Account</span>
+                    <div>
+                      <a id="myLink" href="#" onclick="boards();"><span>Boards</span></a>
+                      <a id="myLink" href="#" onclick="logout();return false;"><span>Logout</span></a>
+                    </div>
+                  </a>`
 let button = document.getElementById("loginButton")
 if (localStorage.getItem("isLogged") === "true"){
-  button.innerHTML = logout
+  button.innerHTML = htmlLogout
 }
+else{
+  button.innerHTML = htmlLogin
+}
+
+function logout(){
+  localStorage.removeItem("isLogged");
+  location.reload();
+}
+function boards(){
+  let user = localStorage.getItem("user") 
+  window.location.replace(`http://${global.apiDirection}/user/${user}/boards`);
