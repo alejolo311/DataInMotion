e4a6c575e7b3837a053ee6654032dffdada0b6e1
Danucas <danrodcastillo1994@gmail.com>
2020-07-16 08:54:41 -0500

    added WebDriverWait to contact input

--- models/whatsapp_web.py
+++ models/whatsapp_web.py
@@ -164,6 +164,8 @@ class WebWhastapp():
                 contact = WebDriverWait(self.driver, 10).until(
                     EC.presence_of_all_elements_located((By.XPATH, xpath)))[0]
                 self.remove_verify()
+                self.driver.execute_script(
+                  'arguments[0].style.backgroundColor = "red";', contact)
                 contact.click()
                 time.sleep(2)
                 break
